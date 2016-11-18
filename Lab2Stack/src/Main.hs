17f5c56eb0a0cbfef4b6e1aab0d95eaab00b8e01
root <root@LocalDeliveryOnly>
2016-11-18 21:23:18 +0000

    chatList now passed down for message parsing

--- Lab2Stack/src/Main.hs
+++ Lab2Stack/src/Main.hs
@@ -33,18 +33,18 @@ connLoop s mvar chatList = do
     (soc, socA) <- accept s
     id <- myThreadId
     mval <- takeMVar mvar
-    if (mval <=8)
+    if (mval <=100)
         then putMVar mvar (mval+1)
         else putMVar mvar mval
-    if (mval <=8)
-        then forkIO $ handleConn mvar (soc, socA) id
+    if (mval <=100)
+        then forkIO $ handleConn mvar (soc, socA) id chatList
         else forkIO $ close soc
     connLoop s mvar chatList
 
-handleConn :: MVar Int -> (Socket, SockAddr) -> ThreadId -> IO ()
-handleConn mvar (s, sa) id = do 
+handleConn :: MVar Int -> (Socket, SockAddr) -> ThreadId -> MVar [(Socket, String, String)] -> IO ()
+handleConn mvar (s, sa) id chatList = do 
     input <- recv s 4096
-    parseMessage s id (B8.unpack input)
+    parseMessage s id (B8.unpack input) chatList
     mval <- takeMVar mvar
     tid <- myThreadId
     if (length (B8.unpack input) == 0)
@@ -52,18 +52,18 @@ handleConn mvar (s, sa) id = do
         else putMVar mvar mval
     if (length (B8.unpack input) == 0)
         then (close s) >> (killThread tid)
-        else handleConn mvar (s, sa) id
+        else handleConn mvar (s, sa) id chatList
 
 
-parseMessage :: Socket -> ThreadId -> String -> IO ()
-parseMessage s id [] = do
+parseMessage :: Socket -> ThreadId -> String -> MVar [(Socket, String, String)] ->  IO ()
+parseMessage s id [] _ = do
     threadDelay 10000
-parseMessage s id ('K':'I':'L':'L':'_':'S':'E':'R':'V':'I':'C':'E':_) = do
+parseMessage s id ('K':'I':'L':'L':'_':'S':'E':'R':'V':'I':'C':'E':_) _ = do
     putStrLn "Shutting Down"
     throwTo id ThreadKilled
-parseMessage s _  ('H':'E':'L':'O':m) = do
+parseMessage s _  ('H':'E':'L':'O':m) _ = do
     name <- getSocketName s
     send s  (B8.pack $ ("HELO" ++ m ++ "IP:" ++ ((splitOn ":" (show name))!!0) ++ "\nPort:"++ ((splitOn ":" (show name))!!1) ++ "\nStudentID:13327420\n"))
     putStrLn "Helo text sent"
-parseMessage s _ m = do
+parseMessage s _ m _ = do
     putStrLn ("some other message recieved(" ++ m ++ ")")
